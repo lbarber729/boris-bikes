a0e3ee4faeb941a7e4583bb0d3610bc322a05013
Student <student@Makerss-MacBook-Pro.local>
2019-05-02 15:02:51 +0100

    added error message for full capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,15 +3,22 @@ require 'bike'
 describe DockingStation do
   describe '#release_bike' do
     it "raises an error when there are no bikes available" do
-
       expect{subject.release_bike}.to raise_error "No bikes available"
+    end
+    it "returns bike if bike is available" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to be_an_instance_of(Bike)
+    end
+
+  end
+
+  describe '#dock' do
+    it "raises an error when the docking station is full" do
+      subject.dock(Bike.new)
+      expect{subject.dock Bike.new}.to raise_error "Docking station is full"
     end
   end
 
-
   it {is_expected.to respond_to(:dock).with(1).argument}
-
-
-#it "raise an error if docking_station is empty" do
-  #   docking_station.release_bike = nil
 end
